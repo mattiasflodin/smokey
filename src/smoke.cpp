ee58dec6a5cb09811100549bb734ab86189de95e
Mattias Flodin <git@codepentry.com>
2014-12-20 14:28:26 +0100

    Show a black window

--- src/smoke.cpp
+++ src/smoke.cpp
@@ -1,10 +1,41 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <gl/GL.h>
+
+#include <stdexcept>
+
+class glfw_context {
+public:
+    glfw_context()
+    {
+        if(!glfwInit())
+            throw std::runtime_error("cannot initialize GLFW");
+    }
+    ~glfw_context()
+    {
+        glfwTerminate();
+    }
+};
 
 int main()
 {
-    if (!glfwInit())
-    {
+    class glfw_context glfw;
+    
+    auto window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if(!window)
         return 1;
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if(GLEW_OK != err)
+        return 1;
+
+    GLuint bufferName = -1;
+    glGenBuffers(1, &bufferName);
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     return 0;
 }
