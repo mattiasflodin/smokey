f613e8c56b225fa0a9e89cb980c5d44e240027d1
Mattias Flodin <git@codepentry.com>
2014-12-26 17:01:39 +0100

    wip

--- src/valarray.hpp
+++ src/valarray.hpp
@@ -1,10 +1,48 @@
-template <typename T>
+#include <future>   // async
+
+namespace valarray_impl
+{
+    struct add
+    {
+
+    };
+    template <class T, class Op>
+    struct binop
+    {
+        T lhs;
+        T rhs;
+
+        std::size_t size() const
+        {
+            return lhs.size();
+        }
+    };
+}
+
+template <class T>
 class valarray {
 public:
     valarray(std::size_t size) :
         _p(new T[size])
     {
-        std::async()
+    }
+    ~valarray()
+    {
+        delete[] _p;
     }
 
+    template <class Op>
+    valarray(Op const& op)
+    {
+        op.size()
+    }
+
+private:
+    T* _p;
 };
+
+template <class T>
+valarray_impl::binop<T, valarray_impl::add> operator+(valarray<T> const& lhs, valarray<T> const& rhs)
+{
+    return valarray_impl::binop<T, valarray_impl::add>(lhs, rhs);
+}
